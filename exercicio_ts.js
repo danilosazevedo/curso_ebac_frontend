151ae22d8adaf2ae04118e07989a9e749ee31ba6
danilosazevedo <danilo-sa@live.com>
2023-02-08 11:28:40 -0300

    Inicial

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplicar(10, 5));
+function dizOla(v1, v2) {
+    return v1 + v2;
+}
+console.log(dizOla("Olá ", "Danilo"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicar(n1:number, n2:number) {
+    return n1 * n2;
+}
+console.log(multiplicar(10,5))
+
+
+function dizOla(v1:string, v2:string) {
+    return v1 + v2;
+}
+console.log(dizOla("Olá ","Danilo"))
